1969ba1d07344444d4a7a2c014a17634b149183f
wildenali <wildeeeeen@gmail.com>
2020-01-10 21:45:37 +0700

    Memahami apa itu String[] dalam Main

---
+++ Java 04 - Memahami String []/src/wilden/tutorial/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wilden.tutorial;
+
+/**
+ *
+ * @author wilden
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        String[] adalah input yg bisa kita masukan ketika mengeksekusi java
+        contoh
+        */
+        
+        System.out.println("ini apa ya");
+        
+    }
+    
+}
