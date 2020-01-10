83344dd8583563f252e993080ad5ab8e40ae1614
wildenali <wildeeeeen@gmail.com>
2020-01-10 08:39:14 +0700

    pendahuluan java

---
+++ Java 01 - Pendahuluan/Main.java
@@ -0,0 +1,12 @@
+// public class -> keywords, publis -> keywords visibility
+// Main itu adalah identifier dari class nya,
+// penamaan nya harus sama file yg di save,
+// misal Main.java makan nama class nya adalah Main,
+// dan huruf depannya harus besar 
+public class Main{  // body si program java
+    
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+    
+}
---
+++ Java 01 - Pendahuluan/OtongTukangCilok.java
@@ -0,0 +1,15 @@
+public class OtongTukangCilok{
+    
+    // tempat dimana program dimulai
+    public static void main(String[] args){
+        // public static adalah keyword untuk java, dan dibuka untuk umum
+        // main harus ada karena bawaan defaultnya java, jgn di ubah
+
+        System.out.print("Otong Tukang Cilok");
+        System.out.print("Otong Tukang Cilok");
+        System.out.println();
+        System.out.println("Otong Tukang Cilok");
+        System.out.println("Otong Tukang Cilok");
+    }
+
+}
