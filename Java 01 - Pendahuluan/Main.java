e291b6ebd456fef1dd207406d122f2c250b85dde
wildenali <wildeeeeen@gmail.com>
2020-01-10 09:05:47 +0700

    cara eksekusi file program dengan terminal

--- Java 01 - Pendahuluan/Main.java
+++ Java 01 - Pendahuluan/Main.java
@@ -10,3 +10,24 @@ public class Main{  // body si program java
     }
     
 }
+
+/*
+Cara Ekeskusi program nya
+
+ketik di terminal dengan directory pada file tersebut (misalnya nama filenya Main.java):
+
+1. javac Main.java
+2. akan muncul Main.class
+3. java Main
+4. akan muncul hasilnya
+
+
+
+ketik di terminal dengan directory pada file tersebut (misalnya nama filenya OtongTukangCilok.java):
+
+1. javac OtongTukangCilok.java
+2. akan muncul OtongTukangCilok.class
+3. java OtongTukangCilok
+4. akan muncul hasilnya
+
+*/
