3001265b90ab664968e1bcb0c441775139fb276f
wildenali <wildeeeeen@gmail.com>
2020-01-10 21:47:01 +0700

    Variable int,char,float,double,long,dll

---
+++ Java 05 - Variable/src/wilden/tutorial.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wilden;
+
+/**
+ *
+ * @author wilden
+ */
+public class tutorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        System.out.println("nilai a = " + a);
+        
+        a = 20;
+        System.out.println("nilai a baru = " + a);
+        
+        int b;
+        b = 7;
+        System.out.println("nilai b = " + b);
+    }
+}
