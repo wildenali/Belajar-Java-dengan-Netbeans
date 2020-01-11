de9f664ec0d6f3787163824985b61c3c2b47aa8c
wildenali <wildeeeeen@gmail.com>
2020-01-11 17:50:49 +0700

    oprtor assignment itu +=, -=, *=, /-, %=

---
+++ Java 10 - Operator Assignments/src/wilden/belajar/Main.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wilden.belajar;
+
+/**
+ *
+ * @author wilden
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Operator Assignment
+        
+        // 1. Assignment Penjumlahan
+        int a = 3;
+        a += 10;    // a = a + 10;
+        System.out.println("(JUMLAH) Nilai a adalah " + a);
+        System.out.println();
+        
+        // 2. Assignment Pengurangan
+        int b = 3;
+        b -= 10;    // b = b - 10;
+        System.out.println("(KURANG) Nilai b adalah " + b);
+        System.out.println();
+        
+        // 3. Assignment Perkalian
+        int c = 50;
+        c *= 30;    // c = c * 10;
+        System.out.println("(KALI) Nilai c adalah " + c);
+        System.out.println();
+        
+        // 4. Assignment Pembagian
+        int d = 85;
+        d /= 10;    // d = d / 10;
+        System.out.println("(BAGI) Nilai d adalah " + d);
+        System.out.println();
+        
+        // 5. Assignment Modulus
+        int e = 10;
+        e %= 7;
+        System.out.println("(MODULUS) Nilai e adalah " + e);
+        System.out.println();
+        
+    }
+    
+}
