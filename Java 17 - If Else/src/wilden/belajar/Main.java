c6f561c495033b2086f2c86c7b183c1553118eb8
wildenali <wildeeeeen@gmail.com>
2020-01-13 21:03:44 +0700

    if else if

---
+++ Java 17 - If Else/src/wilden/belajar/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wilden.belajar;
+
+/**
+ *
+ * @author wilden
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // tutorial if else if statement
+        
+        int a = 5;
+        
+        if(a == 5){
+            System.out.println("Ini adalah aksi 1");
+        }else if(a == 10){
+            System.out.println("Ini adalah aksi 2");
+        }else{
+            System.out.println("Ini adalah aksi default");
+        }
+    }
+    
+}
